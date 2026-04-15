9b0f19e3207c6a19089a0db95571a4528c21cc6e
vic <vic@vixac.com>
2026-04-15 12:24:42 +0100

    log rest calls optionally

--- bullet/rest_bullet/rest_client.go
+++ bullet/rest_bullet/rest_client.go
@@ -15,14 +15,27 @@ type RestClient struct {
 	*util.FirbolgClient
 }
 
-func NewRestClient(baseURL string, space store_interface.TenancySpace) *RestClient {
-	return &RestClient{
-		FirbolgClient: util.NewFirbolgClient(strings.TrimRight(baseURL, "/"), int64(space.AppId), space.TenancyId),
+// Option configures a RestClient.
+type Option func(*util.FirbolgClient)
+
+// WithHTTPClient sets a custom HTTP client.
+func WithHTTPClient(c *http.Client) Option {
+	return func(f *util.FirbolgClient) {
+		f.HTTPClient = c
 	}
 }
 
-func NewRestClientWithHTTPClient(baseURL string, space store_interface.TenancySpace, httpClient *http.Client) *RestClient {
-	client := NewRestClient(baseURL, space)
-	client.HTTPClient = httpClient
-	return client
+// WithLogger sets a logger that will print each request and response.
+func WithLogger(l util.Logger) Option {
+	return func(f *util.FirbolgClient) {
+		f.Logger = l
+	}
+}
+
+func NewRestClient(baseURL string, space store_interface.TenancySpace, opts ...Option) *RestClient {
+	fc := util.NewFirbolgClient(strings.TrimRight(baseURL, "/"), int64(space.AppId), space.TenancyId)
+	for _, o := range opts {
+		o(fc)
+	}
+	return &RestClient{FirbolgClient: fc}
 }
